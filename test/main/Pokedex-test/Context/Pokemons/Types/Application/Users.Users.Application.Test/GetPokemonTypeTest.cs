b8fc5eec537a29279daf34c16636b842e96e9508
Lumbar <eduardojgm_@hotmail.com>
2021-04-20 16:12:17 +0200

    Unit tests feature

---
+++ test/main/Pokedex-test/Context/Pokemons/Types/Application/Users.Users.Application.Test/GetPokemonTypeTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Users.Users.Application.Test
+{
+    [TestClass]
+    public class GetPokemonTypeTest
+    {
+        [TestMethod]
+        public void TestGetPokemonType()
+        {
+
+
+
+            //Assert.AreEqual(
+            //    new FileInfo(Path.Combine(_assetsDirectory, "output-interval-bottomleft-position.pdf")).Length,
+            //    new FileInfo(_outputPdf).Length);
+        }
+    }
+}
